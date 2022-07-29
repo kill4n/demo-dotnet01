a428943eefdbf357cdd11050f7f4222c0bac0850
Crhistian <crhistian.segura@gmail.com>
2022-07-28 19:15:43 -0500

    Read Env vars and pass to front

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,15 +5,20 @@ namespace demo_dotnet01.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+	private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+	public IndexModel(ILogger<IndexModel> logger)
+	{
+		_logger = logger;
+	}
 
-    public void OnGet()
-    {
+	public void OnGet()
+	{
+		var data = Environment.GetEnvironmentVariable("DATAURL");
 
-    }
+        ViewData["data"] = data;
+
+	}
+
+
 }
